b09963c62c1cb14cf48d99755d8a5b58748bd838
Maksim <akiam@mail.ru>
2017-01-12 09:39:30 +0300

    Seems like done

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(function () {
+
+    //Number animation
+    $('.number__number').each(function () {
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 3000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now)+'%');
+            }
+        });
+    });
+
+    //Menu logic
+    $('.mnu__burger').on('click',function () {
+        if($(this).hasClass('mnu__burger--active')){
+            $(this).removeClass('mnu__burger--active');
+            $('.mnu').animate({opacity: 0},function () {
+                $(this).css('display', '');
+            });
+        }else {
+            $(this).addClass('mnu__burger--active');
+            $('.mnu').animate({opacity: 1}).show();
+            $('body').css({'overflow': 'hidden'})
+        }
+    })
+});
